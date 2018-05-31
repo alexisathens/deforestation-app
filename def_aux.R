011eff26da539020da6647590169c0151adadcaf
lexisathens1 <lexisathens1@gmail.com>
2018-05-31 13:31:31 -0400

    dist function much faster

--- def_aux.R
+++ def_aux.R
@@ -2,6 +2,29 @@
 #helper functions for deforestation program
 #move these functions into C++...
 
+get_dist=function(land,roads,splits=20)
+{
+  roads=seq_points(roads,splits) #sequence road points
+  apply(rdist(land[,1:2],roads),1,min) #take min of each plot's distance to road points
+}
+
+seq_points=function(roads,splits)
+{
+  n=nrow(roads) #number of points to granulate btwn
+  for(i in 1:(n-1))
+  {
+    x=seq(from=roads[i,1],to=roads[i+1,1],length.out=splits)
+    y=seq(from=roads[i,2],to=roads[i+1,2],length.out=splits)
+    temp=data.frame(x=x,y=y)
+    roads<-rbind(roads,temp)
+  }
+  return(roads)
+}
+
+
+
+#outdated code.....
+
 #euclidean distance between plots of land (x,y) and road coords
 euclidean_dist=function(land,roads)
 {
@@ -53,36 +76,4 @@ midpoint=function(pt1,pt2,pts=data.frame(x=pt1$x,y=pt1$y),thresh=1.0)
   return(pts)
 }
 
-#intermediary function for determining proportion of deforested neighbors
-def_neighbors=function(land)
-{
-  land.m=df_to_matrix(land)
-  new.land.m=def_nbors(land.m)
-  nbors=matrix_to_vec(new.land.m)
-  return(nbors)
-}
 
-#converter function to be able to index particular plots rather than searching dataframe
-#create matrix holding deforestation statuses of each plot
-df_to_matrix=function(land)
-{
-  #create land matrix
-  #-1 if off map, 0 if on map & forested, 1 if on map & deforested
-  n=max(land$x,land$y)
-  land.m=matrix(data=-1,nrow=n,ncol=n) #-1 off map, on map otherwise
-  for(i in 1:nrow(land)) #mark as de/forested
-  {
-    land.m[land$x[i],land$y[i]]=ifelse(land$dstatus[i]==1,1,0)
-  }
-  return(land.m)
-}
-
-#convert matrix to vector to append to df
-matrix_to_vec=function(land.m)
-{
-  #assuming for now that land df will be constructed the same as by the expand.grid function
-  #i.e., the order for the x,y coords will be of the same construction
-  land.v <- as.vector(land.m)
-  nbors <- land.v[which(land.v!=-1)] #return value for plots that are "on" map only
-  return(nbors)
-}
