9e6523a6c7f49ae4106b8cfb5ef6f030ca914b77
lexisathens1 <lexisathens1@gmail.com>
2018-05-31 13:33:29 -0400

    updated function calls

--- def_core.R
+++ def_core.R
@@ -1,10 +1,12 @@
 #base deforestation app code
 library(Rcpp)
+library(microbenchmark)
+library(fields) #for rdist
 rm(list=ls(all=TRUE))
 set.seed(1)
 
-source('deforestation-app/def_aux.R') #auxiliary R functions
-sourceCpp('deforestation-app/rcpp_functions.cpp') #auxiliary C++ functions
+source('def_aux.R') #auxiliary R functions #deforestation-app/
+sourceCpp('rcpp_functions.cpp') #auxiliary C++ functions #deforestation-app/
 #source('test_funcs.R') #import test cases here of varying complexity..
 
 #get from UI...
@@ -13,21 +15,35 @@ roads=roads #df of user clicks
 ntime=30 #input$years
 
 #get distance to road points
-land$dist.road=euclidean_dist(land,roads)
-#deforest plots near road
-arb.dist=0.75
-land$dstatus=ifelse(land$dist.road<arb.dist,1,0)
+land$dist.road=get_dist(land,roads)
+arb.dist=0.50 #adjust w/r/t size of land?
+#deforest automatically where road is constructed
+land$dstatus=ifelse(land$dist.road<arb.dist,1,0) #where 1=deforested, 0=forested
 
 for(i in 1:ntime)
 {
   #get proportion of deforested neighbors every iteration
-  land$def.nbors=def_neighbors(land)
+  land$def.nbors=def_nbors(land)
   
-  #BART here?
+  #BART here
   
   #previously...
   tmp=exp(b0+b1*land$dist.road+b2*land$def.nbors)
   prob=tmp/(1+tmp)
   land$dstatus=rbinom(nrow(land),size=1,prob=prob)
 }
+
+###-------
+#profile dist
+land$dist.road
+microbenchmark(
+  euclidean_dist(land,roads), #my func
+  get_dist(land,roads)
+)
+
+#quick def vis
+land.m=df_to_matrix(land)
+land.m
+
+
 
