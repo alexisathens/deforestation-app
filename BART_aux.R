e1704a390b6faa0448e51242a1efa0f11a9b5d02
lexisathens1 <lexisathens1@gmail.com>
2018-06-14 18:23:35 -0400

    gibbs for mu

--- BART_aux.R
+++ BART_aux.R
@@ -12,7 +12,7 @@ classify <- function(dat,dec)
 }
 
 #assign each observation an appropriate terminal node
-assign_term <- function(dat,dec,this.id=1)
+assign_term <- function(dat,dec,this.id=1) #modify so works for any shape/size binary tree
 {
   if(floor(log2(this.id)) > floor(log2(max(dec$node.id)))) { #if beyond internal nodes, assign terminal node
     leaf.id=this.id-length(dec$node.id)
@@ -22,4 +22,18 @@ assign_term <- function(dat,dec,this.id=1)
   } else { #right child
     assign_term(dat,dec,2*this.id+1)
   }
+}
+
+samp_mu <- function(param,dat,k)
+{
+  sigma=param[[2]]
+  L<-sum(dat$term.id==k) #number of observations in leaf
+  nu<-sqrt(1/(L/sigma+1/sigma.mu)) #sd of normal-normal conjugation
+  mean<-((nu/sigma)^2)*sum(dat[which(dat$term.id==k),"y"])
+  return(rnorm(1,mean,nu))
+}
+
+samp_sigma <- function()
+{
+  rinvgamma(1,shape=3,rate=0.9)
 }
